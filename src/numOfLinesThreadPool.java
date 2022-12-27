dbb83d73427435b48238fb67ee2f592c481f89e4
Arad Ben Menashe <aradbm@gmail.com>
2022-12-27 20:56:35 +0200

    added new classes for part 1

---
+++ src/numOfLinesThreadPool.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+public class numOfLinesThreadPool implements Callable<Integer> {
+    private String fileName;
+
+    /**
+     * Constructor
+     * 
+     * @param fileName
+     */
+    public numOfLinesThreadPool(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int numOfLines = 0;
+        String fileName = this.fileName;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while (reader.readLine() != null) {
+                numOfLines++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+        return numOfLines;
+    }
+}
---
+++ src/numOfLinesThreads.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class numOfLinesThreads implements Runnable {
+    private String fileName;
+    private int numOfLines;
+
+    /**
+     * Constructor
+     * 
+     * @param fileName
+     */
+    public numOfLinesThreads(String fileName) {
+        this.fileName = fileName;
+        this.numOfLines = 0;
+    }
+
+    /**
+     * Returns the number of lines in the files in fileNames array
+     * 
+     * @param fileName - array of file names
+     * @return numOfLines - number of lines in all files
+     */
+    public int getNumOfLines() {
+        int numOfLines = 0;
+        String fileName = this.fileName;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while (reader.readLine() != null) {
+                numOfLines++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+        return numOfLines;
+    }
+
+    /**
+     * getter method for numOfLines
+     * 
+     * @return numOfLines
+     */
+    public int getNum() {
+        return numOfLines;
+    }
+
+    /**
+     * run method for runnable class
+     */
+    @Override
+    public void run() {
+        this.numOfLines = getNumOfLines();
+    }
+}
