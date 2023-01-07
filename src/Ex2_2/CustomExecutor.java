fc731b2eb34fa26a217f3f675cfc1d98ebbf85fd
aradbm-dskt <aradbm@gmail.com>
2023-01-07 19:13:23 +0200

    added methods to update max priority

--- src/Ex2_2/CustomExecutor.java
+++ src/Ex2_2/CustomExecutor.java
@@ -10,9 +10,7 @@ public class CustomExecutor extends ThreadPoolExecutor {
     private static final int MAX_THREADS = Runtime.getRuntime().availableProcessors() - 1;
     private static final int MIN_THREADS = Runtime.getRuntime().availableProcessors() / 2;
     private int maxPriority;
-    // ArrayList<Thread> myThreads;
-    public static BlockingQueue<Runnable> workingBlockingQueue = new PriorityBlockingQueue<Runnable>(MIN_THREADS,Collections.reverseOrder());
-    // Collections.reverseOrder() or CustomQueue()
+    public static CustomQueue<Runnable> workingBlockingQueue= new CustomQueue<>(MIN_THREADS,Collections.reverseOrder(),10); // 10 is lowest
 
     /* ********************************* Constructor ********************************* */
     /**
@@ -28,9 +26,10 @@ public class CustomExecutor extends ThreadPoolExecutor {
     /* ********************************* Submit Methods ********************************* */
     public Future<?> submit(Task taskToSubmit) {
          if (taskToSubmit == null) throw new NullPointerException();
-         if (taskToSubmit.getPriority() < maxPriority) {
-             maxPriority = taskToSubmit.getPriority();
-         }
+//         if (taskToSubmit.getPriority() < maxPriority) {
+//             maxPriority = taskToSubmit.getPriority();
+//         }
+         workingBlockingQueue.updatePriority(taskToSubmit.getPriority());
          RunnableFuture<?> futureObj = newTaskFor(taskToSubmit);
          addToQueue(taskToSubmit);
          execute(futureObj);
@@ -50,6 +49,7 @@ public class CustomExecutor extends ThreadPoolExecutor {
      * @return the max priority (which is the lowest value of the tasks' priority in the pool).
      */
     public int getCurrentMax() {
+        this.maxPriority = workingBlockingQueue.getMaxPriority();
         return this.maxPriority;
     }
 
